81df0b6c7c78f2dac9f312498794cd8d69dc59f0
Francisco Weslley <weslleysouza22@gmail.com>
2022-08-15 00:17:31 -0300

    💄feat: Criação de estilo global

---
+++ src/styles/theme/materialUi.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#5FAC87",
+        contrastText: "#FFFFFF",
+      },
+    },
+  });
