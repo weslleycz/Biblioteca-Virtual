4feb77c81180d58b4ef7fccc31f789410d30290f
Francisco Weslley <weslleysouza22@gmail.com>
2022-08-15 15:39:17 -0300

    ✨ feat: Component Hero full

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,27 +1,27 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import { ThemeProvider } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
 import Image from "next/image";
 import useMedia from "use-media";
 import heroImg from "../../assets/heroImg.svg";
+import { theme } from "../../styles/theme/materialUi";
 import Styles from "./styles.module.scss";
 
 export const Hero = () => {
     const isWide = useMedia({ minWidth: 720 });
     return (
         <>
-            <Container maxWidth="lg" sx={{marginTop: 2}}>
-                <Box sx={{ flexGrow: 1 }}>
+            <Container maxWidth="lg" sx={{ marginTop: 2 }}>
+                <Box sx={{ flexGrow: 2 }}>
                     <Grid container spacing={1} columns={isWide ? 16 : 5}>
                         <Grid xs={8}>
                             <Container
                                 sx={{
                                     textAlign: "center",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    display: "flex",
-                                    marginTop: 5,
+                                    marginTop: 7,
                                 }}
                             >
                                 <Typography
@@ -29,18 +29,45 @@ export const Hero = () => {
                                         justifyContent: "center",
                                         textAlign: "center",
                                     }}
-                                    variant="h2"
+                                    variant="h3"
                                     gutterBottom
                                     component="div"
                                 >
                                     <strong>
-                                        Bem vindo a sua{" "}
+                                        Bem-vindo a sua{" "}
                                         <strong className={Styles.highlighted}>
                                             biblioteca
                                         </strong>{" "}
                                         online
                                     </strong>
                                 </Typography>
+                                <Typography
+                                    variant="body1"
+                                    sx={{
+                                        textAlign: "center",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        width: "70%",
+                                        margin: "0 auto",
+                                        color: "#2b2a2a",
+                                    }}
+                                    gutterBottom
+                                >
+                                    body1. Lorem ipsum dolor sit amet,
+                                    consectetur adipisicing elit. Quos
+                                    blanditiis tenetur unde.
+                                </Typography>
+                                <ThemeProvider theme={theme}>
+                                    <Button
+                                        variant="contained"
+                                        sx={{ marginTop: 3, width: "50%" }}
+                                        fullWidth={true}
+                                        size="medium"
+                                        disableElevation
+                                    >
+                                        Cadastre-se
+                                    </Button>
+                                </ThemeProvider>
                             </Container>
                         </Grid>
                         <Grid sx={{ justifyContent: "center" }} xs={8}>
