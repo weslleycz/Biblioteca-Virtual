39f1f102cce1a8b160735e860182b44b7e54b112
Francisco Weslley <weslleysouza22@gmail.com>
2022-08-28 16:35:54 -0300

    ✨ feat: utility linux.

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ npm run dev
 # or
 yarn dev
 ```
+Admin credentials:
+
+```bash
+username:admin
+password:admin
+```
+Online admin creation utility:
+
+<a href="./utility/ADM.AppImage"><b>Linux</b></a>
---
+++ prisma/migrations/20220827052105_/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "category" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "pendency" BOOLEAN NOT NULL DEFAULT false,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "idCar" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Books" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "ISBN" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "year" TEXT NOT NULL,
+    "rating" REAL NOT NULL DEFAULT 0,
+    "description" TEXT,
+    "cover" TEXT,
+    "loanId" TEXT,
+    "aDMId" TEXT,
+    CONSTRAINT "Books_loanId_fkey" FOREIGN KEY ("loanId") REFERENCES "Loans" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Books_aDMId_fkey" FOREIGN KEY ("aDMId") REFERENCES "ADM" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Loans" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "startDate" DATETIME NOT NULL,
+    "endDate" DATETIME,
+    "userId" TEXT NOT NULL,
+    "statusLoan" TEXT NOT NULL,
+    CONSTRAINT "Loans_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ADM" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Books_ISBN_key" ON "Books"("ISBN");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ADM_username_key" ON "ADM"("username");
